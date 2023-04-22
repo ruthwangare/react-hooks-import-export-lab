4891e58eec9c0d6666276ed5c0cd8d40b166e1e5
Ruth <rwangare124@gmail.com>
2023-04-22 20:03:13 +0300

    Completed assignment

--- src/data/user.js
+++ src/data/user.js
@@ -1,6 +1,12 @@
+
+
 const username = "Liza";
 const city = "New York";
 
 const image = "https://i.imgur.com/mV8PQxj.gif";
 
 // export all three variables as named exports
+
+export const usernam = "Liza";
+export const ername = "New York";
+export const rname = "https://i.imgur.com/mV8PQxj.gif";
